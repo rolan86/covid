8f5593d0dd25ae8e0ecfb9bcd6a123cb9d92d239
Merryl Rolan DMello <rolan86@gmail.com>
2020-05-06 17:29:03 +0200

    Add dayone route to app

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import json
+
 from flask import Flask
+
+from data_script import get_day_one
 
 
 app = Flask(__name__)
@@ -6,6 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Healthy"
+
+@app.route('/dayone')
+def by_country():
+    return json.dumps(get_day_one("canada"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
